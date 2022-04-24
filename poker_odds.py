762e80cc249b68d072f1259688239748fe10eae4
David Colby <davidcolbycode@gmail.com>
2022-04-24 10:18:10 -0700

    reset

--- poker_odds.py
+++ poker_odds.py
@@ -12,7 +12,9 @@ def letter(event):
     canvas.move(circle, x, y)
 main.bind("<Key>", letter); main.bind("<Left>", left)
 main.bind("<Right>", right); main.bind("<Up>", up); main.bind("<Down>", down)
-main.mainloop(); total = 51 * 50 * 49 * 48 * 47 * 46
+main.mainloop(); totalCombos = 51 * 50 * 49 * 48 * 47 * 46
+def high_card(cards):
+    return total(cards)
 def pair(cards):
     return 0
 def two_pair(cards):
@@ -31,7 +33,11 @@ def straight_flush(cards):
     return 0
 def royal_flush(cards):
     return 0
+def total(cards):
+    return 100
 def pick(key, cards):
+    match key:
+        case [" 1. High Card: "]:    return high_card(cards)
     if (key == " 2. Pair: "):    return pair(cards)
     if (key == " 3. Two (2) Pair: "):    return two_pair(cards)
     if (key == " 4. Three (3) of a Kind: "):    return three_of_a_kind(cards)
@@ -41,41 +47,43 @@ def pick(key, cards):
     if (key == " 8. Four (4) of a Kind: "):    return four_of_a_kind(cards)
     if (key == " 9. Straight Flush: "):    return straight_flush(cards)
     if (key == "10. Royal Flush: "):    return royal_flush(cards)
-    return 100
-def plot_probs(probs, title):
-    ax = plt.subplots(1,1,num=title,figsize=(10,10))[1]
-    hands = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; legend = ""
-    for k, v in probs.items():    legend += k+str(v)+"\n"
-    ax.plot(hands, list(probs.values())[:-1], label=legend)
-    ax.set_ylabel("Probability", fontsize=12); ax.legend(fontsize=12); s=12
-    ax.set_xlabel("Poker Hand",fontsize=12);ax.tick_params("both",labelsize=s)
-    ax.set_title("Probabilities in Percent (%):", fontsize=12); plt.show()
+    if (key == "Total: "):    return total(cards)
+def plot_probabilities(probs, title):
+    hands = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; L=""
+    ax=plt.subplots(1,1,num=title,figsize=(10,10))[1]
+    for k,v in probs.items(): L+=k+str(round(v,4))+"\n"
+    ax.set_xticks(hands); ax.set_yscale("log")
+    ax.plot(hands,list(probs.values())[:-1],label=L)
+    ax.scatter(hands,list(probs.values())[:-1]); s=12
+    ax.set_xlabel("Poker Hand",fontsize=s); ax.legend(fontsize=s)
+    ax.set_ylabel("Probability",fontsize=s);ax.tick_params("both",labelsize=s)
+    ax.set_title("Probabilities in Percent (%):", fontsize=s+0.00); plt.show()
 class PokerOdds:
     def __init__(self):
-        self.cards = []; self.probs = { " 1. High Card: ": total, \
-                       " 2. Pair: ": 3 * 48 * 44 * 40 * 36 * 32, \
-                       " 3. Two (2) Pair: ": 3 * 48 * 3 * 44 * 40 * 36, \
+        self.cards = []; self.probs = { " 1. High Card: ": totalCombos,\
+                       " 2. Pair: ": 3 * 48 * 44 * 40 * 36 * 32,\
+                       " 3. Two (2) Pair: ": 3 * 48 * 3 * 44 * 40 * 36,\
                        " 4. Three (3) of a Kind: ":3 * 2 * 48 * 44 * 40 * 36,\
-                       " 5. Straight: ": 8 * 16 * 16 * 25 * 32 * 28, \
-                       " 6. Flush: ": 12 * 11 * 10 * 9 * 32 * 28, \
-                       " 7. Full House: ": 3 * 2 * 48 * 3 * 44 * 40, \
-                       " 8. Four (4) of a Kind: ": 3 * 2 * 1 * 48 * 44 * 40, \
-                       " 9. Straight Flush: ": 2 * 2 * 2 * 25 * 32 * 28, \
-                       "10. Royal Flush: ": 4*3*2*32*28, "Total: ": total }
+                       " 5. Straight: ": 8 * 16 * 16 * 25 * 32 * 28,\
+                       " 6. Flush: ": 12 * 11 * 10 * 9 * 32 * 28,\
+                       " 7. Full House: ": 3 * 2 * 48 * 3 * 44 * 40,\
+                       " 8. Four (4) of a Kind: ": 3 * 2 * 1 * 48 * 44 * 40,\
+                       " 9. Straight Flush: ": 2 * 2 * 2 * 25 * 32 * 28,\
+                       "10. Royal Flush: ": 24*32*28,"Total: ": totalCombos }
     def holeCards(self):
-        self.probs.update((k,100*v/total) for k,v in self.probs.items())
+        self.probs.update((k,100*v/totalCombos) for k,v in self.probs.items())
         self.probs[" 1. High Card: "] -= sum(list(self.probs.values())[1:-1])
-        plot_probs(self.probs, "Poker Odds Hole Cards Plot:")
+        plot_probabilities(self.probs, "Probabilities with Hole Cards:")
     def flop(self, flop_cards):
-        self.cards+=flop_cards; self.calculate("Poker Odds Flop Cards Plot:")
+        self.cards += flop_cards; self.calc("Probabilities with Flop Cards:")
     def turn(self, turn_card):
-        self.cards+=[turn_card]; self.calculate("Poker Odds Turn Card Plot:")
+        self.cards += [turn_card]; self.calc("Probabilities with Turn Card:")
     def river(self, river_card):
-        self.cards+=[river_card]; self.calculate("Poker Odds River Card Plot")
-    def calculate(self, title):
+        self.cards += [river_card]; self.calc("Probablities with River Card")
+    def calc(self, title):
         self.probs.update((k,pick(k,self.cards))for k,v in self.probs.items())
-        highCard = 100 - sum(list(self.probs.values())[1:-1])
-        self.probs[" 1. High Card: "] = highCard; plot_probs(self.probs,title)
+        self.probs[" 1. High Card: "] -= sum(list(self.probs.values())[1:-1])
+        plot_probabilities(self.probs, title)
 Odds = PokerOdds(); Odds.holeCards(); flopCard1 = ("Clubs", "10")
 flopCard2 = ("Hearts", "Jack"); flopCard3 = ("Clubs", "Queen")
 Odds.flop([flopCard1, flopCard2, flopCard3]); turnCard = ("Spades", "King")
